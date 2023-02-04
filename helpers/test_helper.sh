16635b6204b94ff1643b588c8a65854b11174076
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 20:06:59 +0200

    Updated ssh connection

--- helpers/test_helper.sh
+++ helpers/test_helper.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # TODO add test picking/running all
-timestamp = `date +%y-%m-%d-%H-%M-%S`
+TIMESTAMP=`date +%y-%m-%d-%H-%M-%S`
 if [ $1 == "ansible" ]
 then
-    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> "output-$timestamp.log" 2>&1
-if [ $1 == "salt" ]
+    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> "output-$TIMESTAMP.log" 2>&1
+    scp
+elif [ $1 == "salt" ]
 then
     echo "TODO"
-if [ $1 == "chef" ]
+elif [ $1 == "chef" ]
     echo "TODO"
 else
     echo "Unkown configuration tool" >> output.log
--- libvirt/variables.tf
+++ libvirt/variables.tf
@@ -4,5 +4,7 @@ variable "diskBytes" { default = 1024*1024*1024*10 }
 variable "tool" {
     type = string
     # Change your tool name here
+    # Supported ansible, salt, chef
     default = "ansible"
 }
+
