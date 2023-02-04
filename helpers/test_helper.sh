67e8216e92a253754c553c8ce5d963de9569a922
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 21:41:05 +0200

    Updated path to work

--- helpers/test_helper.sh
+++ helpers/test_helper.sh
@@ -12,7 +12,7 @@ fi
 
 if [ $1 == "ansible" ]
 then
-    (time ansible-playbook /root/kursinis-benchmark/test/ansible/"$TEST") >> "$FILENAME" 2>&1
+    (time ansible-playbook "$TEST") >> "$FILENAME" 2>&1
     scp -o StrictHostKeyChecking=no -P 9889 -i /etc/ssh/ssh_host_ed25519_key "$FILENAME" o0d4@192.168.122.1:/home/o0d4/Programming/kursinis-benchmark/logs/
 elif [ $1 == "salt" ]
 then
