29c0f8b29ea60bff425bb0ce1dc8c7b92a9945c5
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 18:11:26 +0200

    Actually added the elper

---
+++ helpers/test_helper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $1 == "ansible" ]
+then
+    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) > output.log 2>&1
+else
+    echo "Unkown configuration tool"
+fi
