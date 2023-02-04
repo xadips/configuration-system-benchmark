b99e66a8fb0b3f5ded34c9288b57d9fed9fa13a9
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 18:15:21 +0200

    Output even if failed

--- helpers/test_helper.sh
+++ helpers/test_helper.sh
@@ -2,7 +2,7 @@
 
 if [ $1 == "ansible" ]
 then
-    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) > output.log 2>&1
+    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> output.log 2>&1
 else
-    echo "Unkown configuration tool"
+    echo "Unkown configuration tool" >> output.log
 fi
