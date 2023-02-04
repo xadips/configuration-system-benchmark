25c4c0be88c17fc94f9a326dc4e07f2adc31c4d4
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 20:09:10 +0200

    transfer log file

--- helpers/test_helper.sh
+++ helpers/test_helper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # TODO add test picking/running all
 TIMESTAMP=`date +%y-%m-%d-%H-%M-%S`
+FILENAME="output-$TIMESTAMP.log"
 if [ $1 == "ansible" ]
 then
-    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> "output-$TIMESTAMP.log" 2>&1
-    scp
+    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> "$FILENAME" 2>&1
+    scp -P 9889 -i /etc/ssh/ssh_host_ed25519_key "$FILENAME" o0d4@192.168.122.1:/home/o0d4/Programming/kursinis-benchmark/logs/
 elif [ $1 == "salt" ]
 then
     echo "TODO"
