5f7d6c4a6510b9113962392eeee8f2c23fe69976
spidax <29689012+xadips@users.noreply.github.com>
2023-02-04 21:32:34 +0200

    Updated test running

--- helpers/test_helper.sh
+++ helpers/test_helper.sh
@@ -2,9 +2,17 @@
 # TODO add test picking/running all
 TIMESTAMP=`date +%y-%m-%d-%H-%M-%S-%3N`
 FILENAME="$1-output-$TIMESTAMP.log"
+
+if [ -z "$2" ]
+then
+    TEST = "*.yml"
+else
+    TEST = `ls /root/kursinis-benchmark/test/ansible/$2-*.yml`
+fi
+
 if [ $1 == "ansible" ]
 then
-    (time ansible-playbook /root/kursinis-benchmark/test/ansible/1-users.yml) >> "$FILENAME" 2>&1
+    (time ansible-playbook /root/kursinis-benchmark/test/ansible/"$TEST") >> "$FILENAME" 2>&1
     scp -o StrictHostKeyChecking=no -P 9889 -i /etc/ssh/ssh_host_ed25519_key "$FILENAME" o0d4@192.168.122.1:/home/o0d4/Programming/kursinis-benchmark/logs/
 elif [ $1 == "salt" ]
 then
