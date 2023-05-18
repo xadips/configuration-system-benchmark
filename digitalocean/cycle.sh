6d345d28262fae6611239ad3d65949e230efe740
xadips <29689012+xadips@users.noreply.github.com>
2023-05-19 02:38:02 +0300

    Cycle script multiple times

--- digitalocean/cycle.sh
+++ digitalocean/cycle.sh
@@ -1,19 +1,24 @@
 #!/bin/zsh
 # Simple script to re-run testing quicker
-rm -f timings.txt
-terraform plan -var-file="temp.tfvars"
-touch timings.txt
-ncat -k -l -p 9566 >> timings.txt &
-terraform apply -var-file="temp.tfvars" -auto-approve
-VM_COUNT=$(grep vm_count temp.tfvars | awk {'print $3'})
-ANS_COUNT=0
-while [[ "$ANS_COUNT" != "$VM_COUNT" ]]
+for i in {1.."$1"}
 do
-    sleep 3
-    ANS_COUNT=$(cat timings.txt | wc -l)
-    echo "$ANS_COUNT" 
-    echo "$VM_COUNT"
+    rm -f timings.txt
+    touch timings.txt
+    ncat -k -l -p 9566 >> timings.txt &
+    terraform plan -var-file="temp.tfvars"
+    terraform apply -var-file="temp.tfvars" -auto-approve
+    VM_COUNT=$(grep vm_count temp.tfvars | awk {'print $3'})
+    ANS_COUNT=0
+    while [[ "$ANS_COUNT" != "$VM_COUNT" ]]
+    do
+        sleep 3
+        ANS_COUNT=$(cat timings.txt | wc -l)
+        echo "$ANS_COUNT" 
+        echo "$VM_COUNT"
+    done
+    echo "done"
+    terraform destroy -var-file="temp.tfvars" -auto-approve
+    cat timings.txt >> result.txt
+    rm -f timings.txt
+    killall -q ncat
 done
-killall -q ncat
-echo "done"
-terraform destroy -var-file="temp.tfvars" -auto-approve
