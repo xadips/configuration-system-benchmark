06a7676d9ab28729ff59666830d5280a95e22141
xadips <29689012+xadips@users.noreply.github.com>
2023-05-22 03:52:20 +0300

    comment update

--- libvirt/base-volume.tf
+++ libvirt/base-volume.tf
@@ -9,7 +9,7 @@ resource "libvirt_pool" "debian" {
 resource "libvirt_volume" "base-debian-qcow2" {
   name   = "base-debian-qcow2"
   pool   = libvirt_pool.debian.name
-  # Testing framework was made with debian-LTS 20.04 in mind
+  # Testing framework was made with debian 10 in mind
   source = "https://cloud.debian.org/images/cloud/buster/20230504-1370/debian-10-genericcloud-amd64-20230504-1370.qcow2"
   format = "qcow2"
 }
